910e536e42ed47726a98bb144ed404e3bd383c70
klasocki <karolasocki@gmail.com>
2018-12-15 19:14:38 +0100

    Add functions to list Attendees for a given day and workshop

--- sql/functions.sql
+++ sql/functions.sql
@@ -39,9 +39,9 @@ CREATE FUNCTION Balance(@ConferenceReservationID INT)
 AS
 BEGIN
   RETURN
-    (SELECT (SELECT SUM(Amount)
+    (SELECT (SELECT ISNULL(SUM(Amount), 0)
              FROM Payments
-             WHERE ConferenceReservationID = @ConferenceReservationID) - (PriceToPayForWorkshops + PriceToPayForEntries)
+             WHERE ConferenceReservationID = @ConferenceReservationID) - dbo.ReservationCost(@ConferenceReservationID)
      FROM ReservationDetails
      WHERE @ConferenceReservationID = ReservationID)
 end
@@ -49,21 +49,50 @@ GO
 
 CREATE FUNCTION DayPlacesLeft(@DayID INT)
   RETURNS INT
-AS BEGIN
+AS
+BEGIN
   RETURN (
     SELECT (SELECT PlaceLimit FROM Days WHERE ID = @DayID) -
-           (SELECT SUM(PlaceCount) FROM DayReservations
-             WHERE DayID = @DayID)
-    )
+           (SELECT SUM(PlaceCount)
+            FROM DayReservations
+            WHERE DayID = @DayID)
+  )
 end
 GO
 
 CREATE FUNCTION ReservationCost(@ReservationID INT)
   RETURNS NUMERIC(10, 2)
-AS BEGIN
-  RETURN (SELECT PriceToPayForEntries + PriceToPayForWorkshops
-  FROM ReservationDetails)
+AS
+BEGIN
+  RETURN (SELECT ISNULL(PriceToPayForEntries + PriceToPayForWorkshops, 0)
+          FROM ReservationDetails
+          WHERE ReservationID = @ReservationID)
 end
 GO
 
+CREATE FUNCTION DayAttendees(@DayID INT)
+  RETURNS TABLE
+    AS RETURN
+          (
+            SELECT ReservationID, DayReservationID, FirstName, LastName, Email, IsStudent, StudentCardNum
+            FROM Attendees A
+                   JOIN AttendeesDay AD on A.ID = AD.AttendeeID
+                   JOIN DayReservations DR on AD.DayReservationID = DR.ID
+            WHERE DayID = @DayID
+          )
+GO
+
+CREATE FUNCTION WorkshopAttendees(@WorkshopID INT)
+  RETURNS TABLE AS
+    RETURN
+          (
+            SELECT ReservationID, WorkshopReservationID, FirstName, LastName, Email, IsStudent, StudentCardNum
+            FROM Attendees A
+                   JOIN AttendeesDay AD on A.ID = AD.AttendeeID
+                   JOIN AttendeesWorkshop AW ON AD.ID = AW.AttendeeDayID
+                   JOIN WorkshopReservations WR on AW.WorkshopReservationID = WR.ID
+                   JOIN DayReservations DR on WR.DayReservationID = DR.ID
+            WHERE WorkshopID = @WorkshopID
+          )
+GO
 
