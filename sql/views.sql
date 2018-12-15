ac7f30c9c99cf419de2cbc07def0a80f04179631
klasocki <karolasocki@gmail.com>
2018-12-15 14:50:12 +0100

    Add and refactor views for client payments

--- sql/views.sql
+++ sql/views.sql
@@ -13,8 +13,10 @@ FROM Conferences
 WHERE StartDate > GETDATE()
 
 CREATE VIEW DayReservationDetails AS
-SELECT C.Name                     as ClientName,
+SELECT C.ID as ClientID,
+       C.Name                     as ClientName,
        CR.ID                      as ReservationId,
+       Conf.ID as ConferenceID,
        Conf.Name                  as ConferenceName,
        DayNum,
        PlaceCount - StudentCount  as AdultCount,
@@ -38,36 +40,41 @@ CREATE VIEW ReservationDetails AS
 SELECT ClientID,
        ClientName,
        ReservationID,
+       dbo.isCancelled(ReservationId) as isCancelled,
        ConferenceName,
        ConferenceID,
        SUM(AdultCount * PriceForAdult + StudentCount * (1 - StudentDiscount))
-         AS PriceToPayForEntry,
+         AS PriceToPayForEntries,
        (SELECT SUM(Price * WR.PlaceCount)
         FROM WorkshopReservations WR
                JOIN
              Workshops W on WR.WorkshopID = W.ID
-       JOIN DayReservations R on WR.DayReservationID = R.ID
-        WHERE R.ReservationID = DayResDetails.ReservationID
+               JOIN DayReservations R on WR.DayReservationID = R.ID
+        WHERE R.ReservationID = DayReservationDetails.ReservationID
        ) as PriceToPayForWorkshops
-FROM (SELECT C.ID                       as ClientID,
-             C.Name                     as ClientName,
-             CR.ID                      as ReservationId,
-             Conf.Name                  as ConferenceName,
-             Conf.ID                    as ConferenceID,
-             PlaceCount - StudentCount  as AdultCount,
-             StudentCount,
-             (SELECT TOP 1 Price
-              FROM PriceThresholds
-                     JOIN Days InnD on PriceThresholds.DayID = InnD.ID
-                     JOIN Conferences InnC on InnD.ConferenceID = InnC.ID
-              WHERE PriceThresholds.DayID = D.ID
-                AND (DATEDIFF(day, CR.ReservationDate, StartDate) >= DaysBefore)
-              ORDER BY DaysBefore DESC) as PriceForAdult,
-             StudentDiscount
-      FROM DayReservations DR
-             JOIN ConferenceReservations CR on DR.ReservationID = CR.ID
-             JOIN Clients C on CR.ClientID = C.ID
-             JOIN Conferences Conf on CR.ConferenceID = Conf.ID
-             JOIN Days D on Conf.ID = D.ConferenceID) as DayResDetails
+FROM DayReservationDetails
 GROUP BY ClientID, ClientName, ReservationId, ConferenceName, ConferenceID
 
+CREATE VIEW TopTenClients
+AS
+SELECT TOP 10 ID,
+              Name,
+              (SELECT SUM(PlaceCount)
+               FROM DayReservations
+                      JOIN ConferenceReservations CR on DayReservations.ReservationID = CR.ID
+               WHERE ClientID = Clients.ID
+                 AND Cancelled = 0) as TotalPlacesBooked
+FROM Clients
+ORDER BY TotalPlacesBooked
+
+
+CREATE VIEW ClientDueAmount
+AS
+SELECT ClientID, ClientName,
+       SUM(PriceToPayForEntries + PriceToPayForWorkshops) as MoneyToPay,
+       (SELECT SUM(Amount) FROM Payments
+       JOIN ConferenceReservations CR on Payments.ConferenceReservationID = CR.ID
+         WHERE ReservationDetails.ClientID = CR.ClientID) as MoneyPaid
+FROM ReservationDetails
+WHERE isCancelled = 0
+GROUP BY ClientID, ClientName
