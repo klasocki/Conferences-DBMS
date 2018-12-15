37d75d87b2c82160c6f5348d8ee0eec678db0aa8
klasocki <karolasocki@gmail.com>
2018-12-15 17:08:42 +0100

    Add TooManyReservationAttendees trigger

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -10,6 +10,7 @@ BEGIN
       WHERE DayReservationID = inserted.ID
     end
 end
+GO
 
 CREATE TRIGGER ConferenceCancelled
   ON Conferences
@@ -25,6 +26,7 @@ BEGIN
              WHERE ConferenceReservations.ID = ReservationID) = inserted.ID
     end
 end
+GO
 
 CREATE TRIGGER DayReservationNotInConference
   ON DayReservations
@@ -32,13 +34,49 @@ CREATE TRIGGER DayReservationNotInConference
   AS
 BEGIN
   IF EXISTS(
-    SELECT * FROM inserted
-    JOIN ConferenceReservations CR ON inserted.ReservationID = CR.ID
-    JOIN Days D on inserted.DayID = D.ID
-    WHERE D.ConferenceID != CR.ConferenceID
-    )     --(@ResConfID != @DayConfID)
+      SELECT *
+      FROM inserted
+             JOIN ConferenceReservations CR ON inserted.ReservationID = CR.ID
+             JOIN Days D on inserted.DayID = D.ID
+      WHERE D.ConferenceID != CR.ConferenceID
+    ) --(@ResConfID != @DayConfID)
     BEGIN
       THROW 51000, 'Day of reservation is not a day of the conference associated with this reservation', 1
       ROLLBACK
     end
 end
+GO
+
+CREATE TRIGGER TooManyReservationAttendees
+  ON AttendeesDay
+  AFTER INSERT
+  AS
+BEGIN
+  IF EXISTS(SELECT *
+            FROM inserted
+            WHERE (SELECT PlaceCount
+                   FROM DayReservations
+                   WHERE DayReservations.ID = inserted.DayReservationID)
+              >= (SELECT COUNT(*)
+                  FROM AttendeesDay
+                  WHERE DayReservationID = inserted.DayReservationID)
+       ) OR EXISTS(
+         SELECT *
+         FROM inserted
+         WHERE (SELECT StudentCount
+                FROM DayReservations
+                WHERE DayReservations.ID = inserted.DayReservationID)
+           >= (SELECT COUNT(*)
+               FROM AttendeesDay
+               WHERE DayReservationID = inserted.DayReservationID
+                 AND AttendeesDay.IsStudent = 1)
+       )
+
+
+
+    BEGIN
+      THROW 51000, 'All places of your reservation are already taken', 1
+      ROLLBACK
+    end
+end
+GO
