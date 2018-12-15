ad4ce19bbd34c888676b04bc086e796a5a3516e0
klasocki <karolasocki@gmail.com>
2018-12-15 18:07:41 +0100

    Add indices for names of clients and attendees

--- sql/indices.sql
+++ sql/indices.sql
@@ -1,3 +1,7 @@
-CREATE CLUSTERED INDEX Conf_StartDate_INDEX ON Conferences (StartDate)
+CREATE NONCLUSTERED INDEX Conf_StartDate_INDEX ON Conferences (StartDate)
 CREATE NONCLUSTERED INDEX Conf_Name_INDEX ON Conferences(Name)
+
+CREATE NONCLUSTERED INDEX Attendees_Name_INDEX ON Attendees(FirstName, LastName)
+
+CREATE NONCLUSTERED INDEX Clients_Name_INDEX ON Clients(Name)
 
